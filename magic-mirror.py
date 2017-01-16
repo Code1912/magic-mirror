de431bbfac5bef28861e61081a94f7cfa991e11a
Code1912 <code1912@163.com>
2017-01-16 10:18:16 +0800

    fix bug

--- magic-mirror.py
+++ magic-mirror.py
@@ -3,6 +3,7 @@
 from flask import Flask,render_template
 import task
 import sys
+import random
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -11,9 +12,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html",city="成都" )
+    return render_template("index.html",city="成都",r=random.uniform(1, 20)   )
 
 
 if __name__ == '__main__':
     task.Task().start()
+    app.jinja_env.variable_start_string = "[["
+    app.jinja_env.variable_end_string = "]]"
     app.run()
